261277af4596a6504356b0e232ee33788e1e55c2
vineet-singhal <vineet.12mca108@gmail.com>
2021-05-24 10:16:16 +0530

    build contact us page

---
+++ src/components/contact/ContactUs.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function ContactUs() {
+    return (
+        <div className="contactus">
+            <div>
+                <h1 className="bold">We’re here</h1>
+                <p className="sub-text">Our door is always open for a good cup of coffee.</p>
+            </div>
+            <div className="address">
+                <h1>Our Office</h1>
+                <a href="https://goo.gl/maps/36etXfHjYR52">
+                    <p>Jacob van Lennepkade 334-H</p>
+                    <p>1053 NJ Amsterdam</p>
+                    <p>The Netherlands 🇳🇱</p>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default ContactUs
