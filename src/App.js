4b96b57790fe52562ef465f3cea8e087fe323e02
vineet-singhal <vineet.12mca108@gmail.com>
2021-05-24 10:20:06 +0530

    render to app.js page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
-import HomeContact from "./components/contact/HomeContact";
+import HomeContact from './components/contact/HomeContact';
+import Form from "./components/form/Form";
 
 function App() {
+  const [value, setValue] = useState("")
+
+  const handleSetValue = value1 =>{
+    setValue(value1);
+  }
+
   return (
-    <HomeContact/>
+    <>
+    {value==""?<HomeContact setValue={handleSetValue}/>:<Form value={value} setValue={handleSetValue}/>}
+    </>
   );
 }
 
